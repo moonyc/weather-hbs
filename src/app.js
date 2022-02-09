ec616cb96c58050be260ccfe8407c9c63af4f877
moony carton <moonycarton@gmail.com>
2022-02-09 10:48:40 +0100

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PQRT || 3000
 //define path for express condif
 
 
@@ -116,6 +116,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-   console.log('Server is up on port 3000')
+app.listen(port, () => {
+   console.log('Server is up on port '+ port)
 })
