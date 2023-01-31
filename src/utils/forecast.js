972ef5f412dd5827da3495512ca84a4c9b0a4a86
muni <booscarface@gmail.com>
2023-01-31 16:43:45 +0100

    .env

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,8 @@
 const request = require('request')
-
-/*const url='http://api.weatherstack.com/current?access_key=673159eb43e54ee2baa0f1405959bcb9&query&units=f'
-
-request({url: url, json: true}, (error, response)=> {
-    //console.log(response.body.current)
-    if(error) {
-    console.log('Unable to connect to weather service!')
-    }else if(response.body.error) {
-       console.log('unable to find location')
-    }
-    else{
-        console.log(response.body.current.weather_descriptions[0] + ' and it is currently ' + response.body.current.temperature + ' degres out. it feels like ' + response.body.current.feelslike + " degress out.")
-    }
-})
-*/
+require('dotenv').config()
 
 const forecast = (latitude, longitude , callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=673159eb43e54ee2baa0f1405959bcb9&query="+ latitude + ',' + longitude + "&units=f"
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_KEY}&query=`+ latitude + ',' + longitude + "&units=f"
     request({ url, json:true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather service!', undefined)
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,9 +1,9 @@
 const request =require('request');
+require('dotenv').config()
 
 const geocode = (address, callback) => {
-    // const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address +'.json?access_token=pk.eyJ1IjoibW9vbnljYXJ0b24iLCJhIjoiY2t5dTM0ajdiMDJnazJudDRreDJnMHJ0aSJ9.zceVctMWuJDgrRjq6fLsvA&limit=1'
-    //the encode function it's necessary to elaborate special characters that mean something in the url structure.
-    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibW9vbnljYXJ0b24iLCJhIjoiY2t5dTM0ajdiMDJnazJudDRreDJnMHJ0aSJ9.zceVctMWuJDgrRjq6fLsvA&limit=1'
+    
+    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + `.json?access_token=${process.env.ACCESS_TOKEN}&limit=1`
  
     request({url: url, json: true}, (error, {body}) => {
       if(error) {
@@ -11,7 +11,7 @@ const geocode = (address, callback) => {
       } else if (body.features.length === 0) {
          callback('Unable to find location. Try another search.', undefined)
       } else {
-          //it won't have a vlue for error, it will have only the one for data
+          //it won't have a value for error, it will have only the one for data
         callback(undefined, {
              latitude : body.features[0].center[1],
              longitude :body.features[0].center[0],
